6871a8f10da9910b369038a80af7a040c8aa8c4e
occn8 <ochenwayward@gmail.com>
2020-05-27 19:17:59 +0300

    structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:crimetrack/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,25 +13,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(),
     );
   }
 }
